6367d940b3ff7cd0936e206d9b665e64b5363fe4
Eryk <wnekeryk12@gmail.com>
2019-04-27 23:15:17 -0400

    I was not able to finish

---
+++ keys.js
@@ -0,0 +1,83 @@
+require("dotenv").config();
+
+//vars
+var keys = require("./keys.js");
+var fs = require("fs");
+var request = require("request");
+//var Spotify = require('spotify-web-api-node');
+var Spotify = require('node-spotify-api');
+//creates log.txt file
+var filename = './log.txt';
+//NPM module used to write output to console and log.txt simulatneously
+var log = require('simple-node-logger').createSimpleFileLogger(filename);
+log.setLevel('all');
+
+//argv[2] chooses users actions; argv[3] is input parameter, ie; movie title
+var userCommand = process.argv[2];
+var secondCommand = process.argv[3];
+
+//concatenate multiple words in 2nd user argument
+for (var i = 4; i < process.argv.length; i++) {
+    secondCommand += '+' + process.argv[i];
+}
+
+// Fetch Spotify Keys
+var spotify = new Spotify(keys.spotify);
+
+// Writes to the log.txt file
+var getArtistNames = function (artist) {
+    return artist.name;
+};
+
+// Function for running a Spotify search - Command is spotify-this-song
+var getSpotify = function (songName) {
+    if (songName === undefined) {
+        songName = "What's my age again";
+    }
+
+    spotify.search(
+        {
+            type: "track",
+            query: userCommand
+        },
+        function (err, data) {
+            if (err) {
+                console.log("Error occurred: " + err);
+                return;
+            }
+
+            var songs = data.tracks.items;
+
+            for (var i = 0; i < songs.length; i++) {
+                console.log(i);
+                console.log("artist(s): " + songs[i].artists.map(getArtistNames));
+                console.log("song name: " + songs[i].name);
+                console.log("preview song: " + songs[i].preview_url);
+                console.log("album: " + songs[i].album.name);
+                console.log("-----------------------------------");
+            }
+        }
+    );
+};
+
+//Switch command
+function mySwitch(userCommand) {
+
+    switch (userCommand) {
+
+        case "my-tweets":
+            getTweets();
+            break;
+
+        case "spotify-this-song":
+            getSpotify();
+            break;
+
+        case "movie-this":
+            getMovie();
+            break;
+
+        case "do-what-it-says":
+            doWhat();
+            break;
+    }
