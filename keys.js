c9356e1539d38f5454d9d88ed8db430426e2edea
Eryk <wnekeryk12@gmail.com>
2019-04-27 23:17:41 -0400

    Update

--- keys.js
+++ keys.js
@@ -1,22 +1,21 @@
 require("dotenv").config();
-
-//vars
-var keys = require("./keys.js");
 var fs = require("fs");
 var request = require("request");
-//var Spotify = require('spotify-web-api-node');
+var keys = require("./keys.js");
+
+var filename = './log.txt';
 var Spotify = require('node-spotify-api');
 //creates log.txt file
-var filename = './log.txt';
+var userCommand = process.argv[2];
+var secondCommand = process.argv[3];
 //NPM module used to write output to console and log.txt simulatneously
 var log = require('simple-node-logger').createSimpleFileLogger(filename);
 log.setLevel('all');
 
-//argv[2] chooses users actions; argv[3] is input parameter, ie; movie title
-var userCommand = process.argv[2];
-var secondCommand = process.argv[3];
 
-//concatenate multiple words in 2nd user argument
+
+
+
 for (var i = 4; i < process.argv.length; i++) {
     secondCommand += '+' + process.argv[i];
 }
@@ -24,12 +23,12 @@ for (var i = 4; i < process.argv.length; i++) {
 // Fetch Spotify Keys
 var spotify = new Spotify(keys.spotify);
 
-// Writes to the log.txt file
+
 var getArtistNames = function (artist) {
     return artist.name;
 };
 
-// Function for running a Spotify search - Command is spotify-this-song
+
 var getSpotify = function (songName) {
     if (songName === undefined) {
         songName = "What's my age again";
